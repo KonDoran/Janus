9bd7722085ae0e256b6a9fbf7f26fd311647d864
KonDoran <70944904+KonDoran@users.noreply.github.com>
2025-08-07 17:54:42 +0100

    Initial Creation of Janus Project

---
+++ Source/Janus/Janus.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Janus.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Janus, "Janus" );
